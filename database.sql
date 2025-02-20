01d8a35cc46d2a66a04e94098573d432dd351829
soapiisop <112901274+soapiisop@users.noreply.github.com>
2025-02-20 15:23:16 -0500

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE retriever_essentials_data;
+/*
+    item_name = name of the item
+    storage_quantity = quantity of the item in storage
+    num_sold = number of items sold
+    serving_weight = weight of the item per serving
+    serving_amount = amount of the item per serving
+    max_weight = maximum weight of the item that a student can take
+    max_amount = maximum amount of the item that a student can take
+*/
+CREATE TABLE items (
+    item_name TINYTEXT(500),
+    storage_quantity SMALLINT(255),
+    num_sold MEDIUMINT(200000),
+    serving_weight DECIMAL(65, 2),
+    serving_amount TINYINT(50),
+    max_weight DECIMAL(65, 2),
+    max_amount TINYINT(50)
+);
