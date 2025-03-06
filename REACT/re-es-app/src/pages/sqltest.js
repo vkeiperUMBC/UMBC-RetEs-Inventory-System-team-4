258df7c792d0f2698f283417176b61fad0b50c78
vkeiperUMBC <vkeiper1@umbc.edu>
2025-03-06 16:19:58 -0500

    TEAM-46 began work on sql tesing but need backend to be further developed

--- REACT/re-es-app/src/pages/sqltest.js
+++ REACT/re-es-app/src/pages/sqltest.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import SendUsernameButton from '../components/sendUsernameButton';
 
 export function SqlTest() {
     return (
         <div>
             <h1>SQL Test Page</h1>
             <p>This is a simple default React page for SQL testing.</p>
+            <SendUsernameButton />
         </div>
     );
 };
